559acee57da0eee586cf5214e1b743556b85ed4b
LizOchkaaaa <svrvliza14@gmail.com>
2023-01-31 11:43:16 +0300

    Initial commit

---
+++ fghjk.py
@@ -0,0 +1,11 @@
+import re
+p = "X-):<|=-}O8-}PX-)"
+print(len(re.findall(r'X-' , p)))
+#def K(e):
+    #return len(re.findall(p, e))
+#e1 = "X-)"
+#print(K(e1))
+#print(S('message 8-P<{PX<\;-}|X<O:<{(8-(;<)=-O:<{|X-P=-/X-)=-):<{)X<{|'))
+#print(S('massage rtyfjfjfjijieieff80OX-))))////rthjhgdfgX-);.;<}.}<=+X-)X-}}'))
+#print(S('massage : 01;<)2X-{8()(-)<_)-<+tsdtfghgjhsdds%(->==+X-)X-)X-)X-)X-)'))
+#print(S('massage 1-2%=4;{]-{=|X-PVX-)X-)+;B-)S-)X-(X-(<}8X-)+6-)X-)'))
